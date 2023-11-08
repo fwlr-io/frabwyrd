30eff5a4eec2401a39acf80c72ff039c142fe5df
scottfowler <scott@fwlr.io>
2023-11-09 00:48:35 +1100

    implements api calls for openai and anthropic

---
+++ define_word_claude.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# get key
+apikey=$(<~/dev/ml/anthropic.key)
+
+# make call
+curl --silent \
+     --request POST \
+     --url https://api.anthropic.com/v1/complete \
+     --header 'accept: application/json' \
+     --header 'anthropic-version: 2023-06-01' \
+     --header 'content-type: application/json' \
+     --header "x-api-key: $apikey" \
+     --data '
+{
+  "prompt": "\n\nHuman: Pretend that '$1' is a real English word. Give a dictionary definition of '$1'.\n\nAssistant:",
+  "model": "claude-2",
+  "max_tokens_to_sample": 192,
+  "stream": false
+}
+' | jq '.completion'
---
+++ define_word_gpt3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# get key
+apikey=$(<~/dev/ml/openai.key)
+
+# make call
+curl -s https://api.openai.com/v1/chat/completions \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $apikey" \
+  -d '{
+     "model": "gpt-3.5-turbo",
+     "messages": [{"role": "user", "content": " Pretend that '$1' is a real English word. Give a dictionary definition of '$1' ."}],
+     "temperature": 0.7
+   }' \
+   | jq '.choices[0].message.content'
